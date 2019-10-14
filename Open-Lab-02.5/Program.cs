6835c9db4fdec290053617ee04399591f196ffbf
romankalab <roan.kalab@mail.com>
2019-10-14 22:22:41 +0200

    Initial

---
+++ Open-Lab-02.5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Open_Lab_02._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(IsDivisible(number));
+        }
+        private static bool IsDivisible(int number)
+        {
+            return number % 5 == 0;
+        }
+    }
+}
